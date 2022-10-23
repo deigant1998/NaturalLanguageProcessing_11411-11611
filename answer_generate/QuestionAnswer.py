3c1e4448591749f140be1fb838d07480ca082bb2
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-10-23 11:59:55 -0400

    renamed answer_generate

---
+++ answer_generate/QuestionAnswer.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from transformers import (
+    GPT2LMHeadModel,
+    GPT2Tokenizer,
+    AutoTokenizer,
+    AutoModelWithLMHead,
+    pipeline
+)
+import numpy as np
+import random
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW, get_linear_schedule_with_warmup
+from tqdm import tqdm, trange
+import torch.nn.functional as F
+import csv
+
+class T5AnswerGenerator():
+    def __init__(self):
+        model_name = "MaRiOrOsSi/t5-base-finetuned-question-answering"
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelWithLMHead.from_pretrained(model_name)
+
+    def _construct_prompt(
+        self,
+        question: str,
+        relevant_sentences: list[str]
+    ) -> str:
+            
+        context = str(" ".join(relevant_sentences))
+
+        return f'question: {question} context:{context}'
+
+
+    def answer_question(
+        self,
+        question: str,
+        relevant_sentences: list[str]
+    ) -> str:
+        input = self._construct_prompt(question, relevant_sentences)
+
+        encoded_input = self.tokenizer([input],
+                                    return_tensors='pt',
+                                    max_length=512,
+                                    truncation=True)
+        output = self.model.generate(input_ids = encoded_input.input_ids,
+                                    attention_mask = encoded_input.attention_mask)
+        output = self.tokenizer.decode(output[0], skip_special_tokens=True)
+
+        return output
+
+    def answer_questions(
+        self,
+        questions: list[str],
+        relevant_sentences: list[list[str]]
+    ):
+
+        if len(questions) != len(relevant_sentences):
+            raise ValueError(
+                "Arguments questions and relevant_sentences must have the same length"
+            )
+
+        return [
+            self.answer_question(
+                questions[i], relevant_sentences[i]
+            ) for i in range(len(questions))
+        ]
--- answer_generate/format_dataset.py
+++ answer_generate/format_dataset.py
@@ -1,17 +1,20 @@
 import pandas as pd
 
-def create_qa_dataset(data_path: str) -> pd.DataFrame:
+def create_qa_dataset(data_path: str) \
+-> tuple[list[str], list[list[str]]]:
+
     question_data = pd.read_excel(data_path)
+
+    questions = list(question_data["question"])
+
+    all_relevant_sentences = []
+
     for idx, row in question_data.iterrows():
         relevant_idxs = [int(x) for x in str(row["Valid Sentences"]).split(",")]
         sentences = row["Passage (10 sentences)"].split("\n")
 
         relevant_sentences = [sentences[i] for i in relevant_idxs]
 
-        context = str(" ".join(relevant_sentences))
-        
-        question_data.at[idx, "context"] = context
+        all_relevant_sentences.append(relevant_sentences)
 
-        question_data.at[idx, "prompt"] = f'question: {row["question"]} context:{context}'
-
-    return question_data
+    return (questions, all_relevant_sentences)
