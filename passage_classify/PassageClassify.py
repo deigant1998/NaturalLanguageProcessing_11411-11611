025d12637444fe838da59717df6da9568aef2e04
Deigant Yadava <deigant@deigants-air.mynetworksettings.com>
2022-10-23 11:54:04 -0400

    putting together passage and sentence detection

---
+++ passage_classify/PassageClassify.py
@@ -0,0 +1,52 @@
+import torch
+from sentence_transformers import SentenceTransformer, util
+import stanza
+
+LINES_PER_PASSAGE = 10;
+
+
+class PassageClassifier(torch.nn.Module):
+    def __init__(self, file_name):
+        super(PassageClassifier, self).__init__()
+        
+        stanza.download('en')
+        self.tokenizer = stanza.Pipeline(lang='en', processors='tokenize')
+        self.sentences = self.tokenize_file(self.read_and_clean_file(file_name))
+        
+        
+        self.model = SentenceTransformer('sentence-transformers/multi-qa-mpnet-base-dot-v1')#.to(torch.device("cuda:0"), dtype=torch.float, non_blocking=True)
+        self.sentence_embeddings = [torch.tensor(self.model.encode(sentence)) for sentence in self.sentences]
+        print(f"Generated embeddings for {len(self.sentences)} sentences")
+    
+    def classify(self, question):
+        question_embedding = torch.tensor(self.model.encode(question))
+        dot_scores = sorted({i:util.dot_score(question_embedding, 
+                                   self.sentence_embeddings[i])[0] for i in range(len(self.sentence_embeddings))}.items(), key=lambda x:x[1], reverse=True)
+        print(dot_scores[0])
+        return dot_scores
+    
+    def read_and_clean_file(self, file_name):
+        clean_text = open(file_name).read().replace('\n', '')
+        clean_text = clean_text.replace('=', '')
+        return clean_text
+    
+    def tokenize_file(self, clean_text):
+        stanza_result = self.tokenizer(clean_text)
+        sentences = [stanza_result.sentences[i].text for i in range(len(stanza_result.sentences))]
+        return sentences
+    
+    def find_passage_start_and_end(self, sentence_index, len_sentences):
+        start = max(0, sentence_index - 5)
+        end = min(start + 10, len_sentences)
+        return start, end
+    
+    def get_related_passages(self, question):
+        sentence_number = self.classify(question)
+        result = []
+        start, end = self.find_passage_start_and_end(sentence_number[0][0], len(self.sentences))
+        result.append("\n".join(self.sentences[start: end]))
+        start, end = self.find_passage_start_and_end(sentence_number[1][0], len(self.sentences))
+        result.append("\n".join(self.sentences[start: end])) 
+        return result
+    
+
